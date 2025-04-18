7ab5a92b48c3a1d6b78499da2cc82b5ecb0081fd
Jan van der Laan <r@eoos.dds.nl>
2025-04-18 15:10:02 +0000

    version 0.2.1

--- R/printdescription.R
+++ R/printdescription.R
@@ -2,12 +2,22 @@
 printdescription <- function(x, description = TRUE, all = FALSE, ...) {
   name <- dp_name(x)
   if (!is.null(name)) {
-    cat("[", name, "] ", sep = "")
+    cat("[", c1(bd(name)), "] ", sep = "")
   } else {
     cat("<NAME MISSING> ", sep = "")
   }
+  if (methods::is(x, "fielddescriptor")) {
+    type <- dp_type(x)
+    categories <- if (!is.null(dp_categorieslist(x))) 
+      c2("/categories") else ""
+    if (!is.null(type)) {
+      cat("<", c2(type), categories, "> ", sep = "")
+    } else {
+      cat("<", c2("TYPE MISSING"), "> ", sep = "")
+    }
+  }
   title <- dp_title(x)
-  if (!is.null(title)) cat(title, "\n", sep = "") else cat("\n")
+  if (!is.null(title)) cat(ul(title), "\n", sep = "") else cat("\n")
   descr <- dp_description(x, first_paragraph = TRUE, dots = TRUE)
   if (description && !is.null(descr)) {
     cat("\n", sep = "")
